46844b698fe5b576f7431b74c7504c97e9e1a8e0
HamzaDara <143824309+HamzaDara@users.noreply.github.com>
2023-09-19 12:41:58 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,226 +1,170 @@
-//student.splice(0,2)      delete arry elements.
-//student.splice(1,0)        add elements.
 
-//student.slice(2,5);  copy 
+//////////Assigment//////////Q3
 
-// var info = ["ali", 13, "gb"];
-// info.pop();   delete from last
-// info.push();   add from  last
-// info.shift();
-// info.shift();
-//var convrt = info.join();    make a single string 
+var num = [1,2,3,4,5,6,7,8,9,10];
+for (var i=0; i<num.length; i++ ){
+    console.log( num[i]);
+}
 
 
-// console.log(info);
+var userInput=+prompt("enter the table num");
+var tableLength=+prompt("enter the table length");
 
-// var ary = [];
-// ary[0] = prompt();
-// ary[1] = +prompt();
+for(var i=1; i<=tableLength; i++){
+    document.write(userInput+"x"+i +"="+userInput*i+"<br>")
+}
 
-// if (ary[1] == 13) {
+var fruits=["apple","banan","Grapes","mango"];
+for(var i=0; i<fruits.length; i++){
+    console.log("Element of"+fruits[i]+"","","" +"at"+" "+i);
+}
 
-//     console.log("come here")
 
-// }
 
-// console.log(ary);
+for (var i=0; i<=10; i++ ){
+    console.log(i);
+}
 
 
+for (var i=0; i<=10; i=i+2 ){
+    console.log(i);
+}
 
-//07
-// var adu = ["SSC", "HSC", "BCS", "BS", "Bcom", "Ms", "Mphil", "phd"];
 
-// document.writeln("Qualifications:<br>" + adu)
+for (var i=0; i<=10; i=i+3 ){
+    console.log(i);
+}
 
+for (var i=0; i<=10; i++ ){
+    console.log(i+"k");
+}
 
-//console.log("all is well");
-// var stu = ["ali", "aslam", "khan"];
-// var scr = [320, 230, 480];
-// console.log("score of ali is  " + scr[0] + "  percentage: " + (scr[0] / 500) * 100 + "%");
-// console.log("score of Aslam is  " + scr[1] + "  percentage: " + (scr[1] / 500) * 100 + "%");
-// console.log("score of Khan is  " + scr[2] + "  percentage: " + (scr[2] / 500) * 100 + "%");
+var userInput=("enter your choice");
+var A=["apple","banan","Grapes","mango"];
+for(var i=0; i<fruits.length; i++){
+    console.log("Element of"+fruits[i]+"","","" +"at"+" "+i);
+}
 
+var a=["cacke","samosa","pestries","cookies","sandwich"];
+var userInput=prompt("enter your choice");
+for(var i=0; i<a.length; i++){
+    if(userInput==a[i]){
+        console.log("yes its avalible in 2nd row");
+    }
+}
 
-// 09
-// document.write('Original Array:', colorArray);
+var a=[21,23,45,81,31,22];
+var userInput=prompt("enter your num");
+for(var i=0; i<a.length; i++){
+    if(userInput==3){
+        console.log("The largest Num is 91");
+    }
+}
 
-// // a. Add a color to the beginning of the array
-// const colorToAddAtBeginning = prompt('Enter a color to add to the beginning:');
-// colorArray.unshift(colorToAddAtBeginning);
+var a=[21,23,45,81,31,22];
+var userInput=prompt("enter your num");
+for(var i=0; i<a.length; i++){
+    if(userInput==0){
+        console.log("The smallest Num is 21");
+    }
+}
+ 
+for (var i=0; i<=100; i=i+5 ){
+    console.log(i);
+}
 
-// // Display the updated array
-// document.write('Array after adding color to the beginning:', colorArray);
 
-// // b. Add a color to the end of the array
-// const colorToAddAtEnd = prompt('Enter a color to add to the end:');
-// colorArray.push(colorToAddAtEnd);
 
-// // Display the updated array
-// document.write('Array after adding color to the end:', colorArray);
 
-// // c. Add two more colors to the beginning of the array
-// colorArray.unshift('Purple', 'Pink');
 
-// // Display the updated array
-// document.write('Array after adding two more colors to the beginning:', colorArray);
+//  chapter 17-20
+///////////////////
 
-// // d. Delete the first color in the array
-// colorArray.shift();
+var i = 2;
 
-// // Display the updated array
-// document.write('Array after deleting the first color:', colorArray);
+for(var i=2; i<=10;i++)
+{
+    console.log("welcom boy");
+}
 
-// // e. Delete the last color in the array
-// colorArray.pop();
 
-// // Display the updated array
-// document.write('Array after deleting the last color:', colorArray);
+for (var i = 0; i <= 12; i++) {
+    console.log("increament", i);
+}
 
-// // f. Add a color at a specific index
-// const indexToAddColor = prompt('Enter the index to add a color:');
-// const colorToAddAtIndex = prompt('Enter the color to add at the specified index:');
-// colorArray.splice(indexToAddColor, 0, colorToAddAtIndex);
 
-// // Display the updated array
-// document.write('Array after adding a color at the specified index:', colorArray);
+for (var i = 0; i<=4; i++){
+    console.log(i);
+}
 
-// // g. Delete color(s) from a specific index
-// const indexToDelete = prompt('Enter the index from which to delete color(s):');
-// const numberOfColorsToDelete = prompt('Enter the number of colors to delete:');
-// colorArray.splice(indexToDelete, numberOfColorsToDelete);
 
-// // Display the updated array
-// document.write('Array after deleting color(s) from the specified index:', colorArray);
-
-
-// 09
-// document.write('Original Array:', colorArray);
-
-// // a. Add a color to the beginning of the array
-// const colorToAddAtBeginning = prompt('Enter a color to add to the beginning:');
-// colorArray.unshift(colorToAddAtBeginning);
-
-// // Display the updated array
-// document.write('Array after adding color to the beginning:', colorArray);
-
-// // b. Add a color to the end of the array
-// const colorToAddAtEnd = prompt('Enter a color to add to the end:');
-// colorArray.push(colorToAddAtEnd);
-
-// // Display the updated array
-// document.write('Array after adding color to the end:', colorArray);
-
-// // c. Add two more colors to the beginning of the array
-// colorArray.unshift('Purple', 'Pink');
-
-// // Display the updated array
-// document.write('Array after adding two more colors to the beginning:', colorArray);
-
-// // d. Delete the first color in the array
-// colorArray.shift();
-
-// // Display the updated array
-// document.write('Array after deleting the first color:', colorArray);
-
-// // e. Delete the last color in the array
-// colorArray.pop();
-
-// // Display the updated array
-// document.write('Array after deleting the last color:', colorArray);
-
-// // f. Add a color at a specific index
-// const indexToAddColor = prompt('Enter the index to add a color:');
-// const colorToAddAtIndex = prompt('Enter the color to add at the specified index:');
-// colorArray.splice(indexToAddColor, 0, colorToAddAtIndex);
-
-// // Display the updated array
-// document.write('Array after adding a color at the specified index:', colorArray);
-
-// // g. Delete color(s) from a specific index
-// const indexToDelete = prompt('Enter the index from which to delete color(s):');
-// const numberOfColorsToDelete = prompt('Enter the number of colors to delete:');
-// colorArray.splice(indexToDelete, numberOfColorsToDelete);
-
-// // Display the updated array
-// document.write('Array after deleting color(s) from the specified index:', colorArray);
-
-
-
-//10
-
-// var stu = [320, 230, 480, 120];
-// var sort = stu.sort();
-// console.log(sort);
-
-//11
-
-// var city = ["khi", "isl", "lahore", "skd", "GB"];
-
-// var copy = city.slice(2, 6);
-
-// console.log(copy);
-
-//12
-// var arr = ["this", "is", "my", "cat"];
-// console.log(typeof arr);
-// console.log(arr);
-
-
-// var jo = arr.join();
-
-// console.log(typeof jo);
-// console.log(jo);
-
-//13
-// var cs = ["keyboard", "mouse", "printer", "monitor"];
-// console.log(cs)
-
-
-// cs.shift();
-// console.log("out: \n keyboard");
-
-// console.log(cs);
-// cs.shift();
-// console.log("out: \n mouse");
-
-// console.log(cs);
-
-// cs.shift();
-// console.log("out: \n printer");
-
-// console.log(cs);
-
-// cs.shift();
-// console.log("out: \n monitor");
-
-// console.log(cs);
-
-//14
-// var cs = ["keyboard", "mouse", "printer", "monitor"];
-// console.log(cs)
-
-
-// cs.pop();
-// console.log("out:  monitor");
-
-// console.log(cs);
-// cs.pop();
-// console.log("out: printer");
-
-// console.log(cs);
-
-// cs.pop();
-// console.log("out:  mouse");
-
-// console.log(cs);
-
-// cs.pop();
-// console.log("out:  keyboard");
-
-// console.log(cs);
-
-//15
-// var cell = ["Apple", "Samsung", "Motrola", "Nokia", "Sony", "Vivo"];
-// console.log(cell[2])
-
-// document.writeln("ssss <br" + cell)
+for (var run=0; run<100; run++){
+    console.log("Fassat",run)
+}
+
+
+var counter = 10; 
+const numLoops = 3;
+for (var i = 0; i < numLoops; i++) {
+  console.log("Counter:", counter);
+  counter--;
+}
+
+var myArray = [1, 2, 3, 4, 5,6,7,8,9,10];
+var arrayLength = myArray.length;
+console.log("Number of elements in the array:", arrayLength);
+
+
+var flag = true;
+
+
+const pets = ["dog", "cat", "fish", "rabbit"]; 
+for (let i = 0; i < pets.length; i++) {
+  console.log("Pet:", pets[i]);
+}
+
+for (let i = 1; i <= 10; i++) {
+    if (i === 2) {
+      alert("Counter: " + (i - 1));
+      break; 
+    }
+  }
+
+
+var userNames =[ "Alia", "Amjad", "Aslam", "irfan"];
+var firstName = prompt("enter your first name");
+var nameFound = false ;
+for( var i=0; i < userNames.length; i++){
+    if(firstName === userNames[i]){
+        nameFound=true ;
+       break;
+    }
+}
+if(nameFound){
+    alert("welcome" + firstName);
+} else{
+    alert("pleae enter correct name");
+}
+
+
+
+    var list =["aslam","akram", "amjad"];
+    var userInput=prompt("enter your name");
+    var matchFound = false;
+ for (var i = 0; i < list.length; i++) {
+    if (userInput === list[i]) {
+    alert("Match found");
+    matchFound = true;
+     break;
+    }
+    }
+    
+
+var firstArr =["a","b","c","d","e","f"];
+var secondArr =[1,2,3,4,5,6];
+for( var i=0; i<firstArr.length; i++){
+    for (var j=0; j<secondArr.length; j++){
+        var concatenatedValue = firstArr[i] + secondArr[j];
+        console.log(concatenatedValue);
+    }
+ }
